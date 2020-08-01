0fdcefcc176194c9be4570139f1f7a67a68875cc
Matthieu Mota <matthieumota@gmail.com>
2020-08-01 12:54:42 +0200

    Improve

--- src/Controller/DemoController.php
+++ src/Controller/DemoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,6 +13,16 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DemoController extends AbstractController
 {
+    private $cars;
+
+    public function __construct()
+    {
+        $this->cars = [
+            ['marque' => 'Renault', 'modele' => 'Megane'],
+            ['marque' => 'Peugeot', 'modele' => '206'],
+        ];
+    }
+
     /**
      * @Route("/demo", name="demo")
      */
@@ -26,14 +37,9 @@ class DemoController extends AbstractController
         // Afficher un élément de la session
         dump($session->get('name'));
 
-        $cars = [
-            ['name' => 'Renault'],
-            ['name' => 'Peugeot'],
-        ];
-
         return $this->render('demo/index.html.twig', [
             'controller_name' => 'DemoController',
-            'cars' => $cars,
+            'cars' => $this->cars,
         ]);
     }
 
@@ -61,6 +67,71 @@ class DemoController extends AbstractController
             throw new AccessDeniedHttpException('Interdit');
         }
 
-        return $this->file('./cv.pdf', 'new_file.pdf', ResponseHeaderBag::DISPOSITION_INLINE);
+        return $this->file('../cv.pdf', 'new_file.pdf', ResponseHeaderBag::DISPOSITION_INLINE);
+    }
+
+    /**
+     * @Route("/toto", name="toto")
+     *
+     * Redirige vers /demo
+     */
+    public function toto()
+    {
+        dump('REQUETE SQL');
+        dump('ENVOI UN EMAIL');
+
+        return $this->redirectToRoute('demo');
+    }
+
+    /**
+     * @Route("/event/{slug}")
+     */
+    public function showEvent(Request $request, $slug, LoggerInterface $logger)
+    {
+        // Mes événements
+        $events = ['a', 'b', 'c'];
+
+        if (!in_array($slug, $events)) {
+            // Si le slug n'est pas dans le tableau
+            throw $this->createNotFoundException('Cet événement n\'existe pas.');
+        }
+
+        // On peut logguer des "trucs" dans Symfony
+        $ip = $request->server->get('REMOTE_ADDR');
+        dump(get_class($logger));
+        $logger->info($ip.' a vu l\'événement '.$slug);
+
+        return new Response('<body>'.$slug.'</body>');
+    }
+
+    /**
+     * On va créer deux nouvelles routes :
+     * /voir-session : Afficher le contenu de la clé 'name' dans la session
+     *                 N'affiche rien lors de la première visite sur le site
+     * /mettre-en-session/{name} : Mettre en session la valeur passée dans l'URL
+     */
+
+    /**
+     * @Route("/voir-session", name="show_session")
+     */
+    public function showSession(SessionInterface $session)
+    {
+        dump($session->get('name'));
+
+        return $this->render('demo/show_session.html.twig');
+    }
+
+    /**
+     * @Route("/mettre-en-session/{name}", name="put_session")
+     */
+    public function putSession($name, SessionInterface $session)
+    {
+        // Je mets $name en session
+        $session->set('name', $name); // Equivaut à $_SESSION['name'] = $name;
+
+        // On peut créer un message flash
+        $this->addFlash('success', 'Message de succès');
+
+        return $this->redirectToRoute('show_session');
     }
 }
